3870cca4ea646714a876f2cdeb28166bd6bb9074
student <valiev.ilnur@bk.ru>
2019-11-29 01:47:12 +0300

    не могу разобраться с lastSeenId

--- app.js
+++ app.js
@@ -1,45 +1,90 @@
 const express = require('express');
 const cors = require('cors');
 
+const errorNotFound = { error: 'id.not_found' };
+
+let nextId = 1;
+let posts = [];
+
 const server = express();
 server.use(express.json());
 server.use(cors());
 
-const messages = [];
-let nextId = 1;
+function findPostIndexById(id) {
+    return posts.findIndex(o => o.id === id);
+}
 
-server.get('/messages/:lastSeenId', (req, res) => {
+server.get('/posts/:lastSeenId', (req, res) => {
     const lastSeenId = Number(req.params.lastSeenId);
-    // lastSeend === 0
+    console.log(lastSeenId);
     if (lastSeenId === 0) {
-        res.send(messages.slice(messages.length - 50));
+        res.send(posts.slice(posts.indexOf(posts[posts.length - 5])));
         return;
     }
 
-    const index = messages.findIndex(o => o.id > lastSeenId);
+    const index = posts.findIndex(o => o.id > lastSeenId);
     if (index === -1) {
-        // Bad Request
         res.send([]);
         return;
     }
 
-    res.send(messages.slice(index, index + 50));
+    res.send(posts.slice(index, index + 5));
 });
 
-server.post('/messages', (req, res) => {
-    const {content} = req.body;
-    // const content = req.body.content;
-
-    // no content
-    setTimeout(() => {
-        messages.push({
-            id: nextId++,
-            content
-        });
-
-        res.status(204).end();
-    }, 5000);
-
+server.get('/posts', (req, res) => {
+    res.send(posts);
 });
 
-server.listen(9999);
+server.post('/posts', (req, res) => {
+    const body = req.body;
+    const id = body.id;
+    if (id === 0) {
+        posts = [...posts, { id: nextId++, content: body.content, type: body.type, likes: 0 }];
+        res.send(posts);
+        return;
+    }
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+    posts = posts.map(o => o.id !== id ? o : { ...o, content: body.content, type: body.type });
+    res.send(posts);
+});
+
+server.delete('/posts/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+    posts = posts.filter(o => o.id != id);
+    res.send(posts);
+});
+
+server.post('/posts/:id/likes', (req, res) => { //god
+    const id = Number(req.params.id);
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+    posts = posts.map(o => o.id !== id ? o : { ...o, likes: o.likes + 1 })
+    res.send(posts);
+});
+
+server.delete('/posts/:id/likes', (req, res) => { // god
+    const id = Number(req.params.id);
+    const index = findPostIndexById(id);
+    if (index === -1) {
+        res.status(404).send(errorNotFound);
+        return;
+    }
+    posts = posts.map(o => o.id !== id ? o : { ...o, likes: o.likes - 1 })
+    res.send(posts);
+});
+
+server.listen(process.env.PORT || 9999);
+
+
