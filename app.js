fc1d9020d9ff4fc2046cbe72444c02a4bc9ad439
student <valiev.ilnur@bk.ru>
2019-11-29 15:01:55 +0300

    поправка lastSeenId

--- app.js
+++ app.js
@@ -15,66 +15,24 @@ function findPostIndexById(id) {
 }
 
 
-function getPostsChunk(index) {
-    if (index - 5 >= 0) {
-        return posts.slice(index - 5, index);
-    }
-    if (index - 5 < 0) {
-        return posts.slice(0, index);
-    }
-}
-
-
 server.get('/posts/seenPosts/:lastSeenId', (req, res) => {
     const lastSeenId = Number(req.params.lastSeenId);
-    let postChunkArr;
-    if (lastSeenId === 0) {
-        postChunkArr = posts.length < 5 ? posts : posts.slice(posts.length - 5);
-    } else {
-    // console.log(lastSeenId);
     const index = findPostIndexById(lastSeenId);
     // console.log(index);
-    postChunkArr = getPostsChunk(index);
-    // console.log(postChunkArr);
-    }
+    let postChunkArr;
+    if (lastSeenId === 0) {
+        postChunkArr = posts.slice(posts.length - 5);
 
+    } else if (lastSeenId > 0 && lastSeenId <= 5) {
+        postChunkArr = posts.slice(0, index);
+
+    } else {
+        postChunkArr = posts.slice(index - 5, index);
+    }
+    // console.log(postChunkArr)
     res.send(postChunkArr);
 });
 
-// server.get('/posts/:lastSeenId', (req, res) => {
-//     const lastSeenId = Number(req.params.lastSeenId);
-//     // console.log(lastSeenId);
-//     // lastSeend === 0
-//     if (lastSeenId === 0) {
-//         res.send(posts.slice(posts.indexOf(posts[posts.length - 5])));
-//         return;
-//     }
-
-//     const index = posts.findIndex(o => o.id > lastSeenId);
-//     if (index === -1) {
-//         res.send([]);
-//         return;
-//     }
-
-//     res.send(posts.slice(index));
-// });
-
-// server.get('/posts/:lastSeenId', (req, res) => {
-//     const lastSeenId = Number(req.params.lastSeenId);
-//     console.log(lastSeenId);
-//     if (lastSeenId === 0) {
-//         res.send(posts.slice(posts.indexOf(posts[posts.length - 5])));
-//         return;
-//     }
-
-//     const index = posts.findIndex(o => o.id > lastSeenId);
-//     if (index === -1) {
-//         res.send([]);
-//         return;
-//     }
-
-//     res.send(posts.slice(index +5 ));
-// });
 
 server.get('/posts', (req, res) => {
     res.send(posts);
@@ -108,7 +66,7 @@ server.delete('/posts/:id', (req, res) => {
     res.send(posts);
 });
 
-server.post('/posts/:id/likes', (req, res) => { //god
+server.post('/posts/:id/likes', (req, res) => { 
     const id = Number(req.params.id);
     const index = findPostIndexById(id);
     if (index === -1) {
@@ -119,7 +77,7 @@ server.post('/posts/:id/likes', (req, res) => { //god
     res.send(posts);
 });
 
-server.delete('/posts/:id/likes', (req, res) => { // god
+server.delete('/posts/:id/likes', (req, res) => { 
     const id = Number(req.params.id);
     const index = findPostIndexById(id);
     if (index === -1) {
